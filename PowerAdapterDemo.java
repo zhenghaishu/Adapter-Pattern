e8fb4aa7d963b0d716db42a581f506fda9bd5f01
zhenghaishu <haishu_zheng@163.com>
2013-12-31 16:49:04 +0800

    add keynote to display

--- PowerAdapterDemo.java
+++ PowerAdapterDemo.java
@@ -1,40 +1,44 @@
+/**
+ * @功能 对象适配器模式的具体例子演示
+ * @作者 郑海树
+ * @时间 2013-12-31
+ */
 
-public class PowerAdapterDemo { 
-    public static void main(String[] args) {  
-        MobilePowerAdapter mpa=new MobilePowerAdapter(); 
-        int power = mpa.GetPower10V();
-        System.out.println("Current power is " + power + "V");
-    } 
-} 
+public class PowerAdapterDemo {
+    public static void main(String[] args) {
+        MobilePowerAdapter adpter = new MobilePowerAdapter();
+        int voltage = adpter.GetPower10V();
+        System.out.println("Voltage from adpater is " + voltage + "V");
+    }
+}
 
+interface ITarget {
+    int GetPower10V();
+}
 
-interface ITarget { 
-    int GetPower10V(); 
-} 
- 
- 
-class Power { 
-    int GetPower220V() { 
-         return 220; 
-    } 
-} 
+// 电源，被适配者（Adaptee）
+class Power {
+    int GetPower220V() {
+        return 220;
+    }
+}
 
- 
-class MobilePowerAdapter implements ITarget { 
-    private Power power; 
-    public MobilePowerAdapter() { 
-        this.power = new Power(); 
-    } 
-     
+// 移动电源适配器
+class MobilePowerAdapter implements ITarget {
+    private Power power;
+    public MobilePowerAdapter() {
+        this.power = new Power();
+    }
+    
     @Override
     public int GetPower10V() {
-        int p = power.GetPower220V();
-        System.out.println("Original power is " + p + "V");
+        int v = power.GetPower220V();
+        System.out.println("Voltage from outlet is " + v + "V");
         
-        return change220To10(p);
-    } 
+        return change220To10(v);
+    }
 	
-	public int change220To10(final int p) {
-		return p / 22;
+	public int change220To10(final int voltage) {
+		return voltage / 22;
 	}
 } 
